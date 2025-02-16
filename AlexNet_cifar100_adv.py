0fc9f49382cf31fdcd98aba614631d3f35ad66d5
Jialei Song <95927824+songjialeichenruiyi@users.noreply.github.com>
2025-02-16 17:41:07 +0800

    Update AlexNet_cifar100_adv.py

--- AlexNet_cifar100_adv.py
+++ AlexNet_cifar100_adv.py
@@ -3,57 +3,46 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms, models
-# import ROBY
 import RDI
 import PGD_attack
 import Square_Attack
 import RFGSM
 import numpy as np
 
-# 检查是否有可用的GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(torch.cuda.is_available())
 
-# 加载 torchvision.models 中的 AlexNet 模型，并修改输入层以适应 CIFAR-100 数据集
 class ModifiedAlexNet(nn.Module):
-    def __init__(self, num_classes=100):  # 将类别数量改为 100
+    def __init__(self, num_classes=100):
         super(ModifiedAlexNet, self).__init__()
         self.model = models.alexnet(pretrained=True)
-        # 修改第一层卷积来适应 CIFAR-100 小尺寸输入
         # self.model.features[0] = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
-        # 修改最大池化层，减少池化窗口的大小和步幅
         # self.model.features[2] = nn.Identity()
-        self.model.classifier[6] = nn.Linear(4096, num_classes)  # 修改输出层以适应 CIFAR-100 的类别数量
+        self.model.classifier[6] = nn.Linear(4096, num_classes)
 
     def forward(self, x):
         return self.model(x)
 
     
-
-# 数据预处理：转换为Tensor并进行归一化
 transform = transforms.Compose([
-    # 改变图像大小
     transforms.Resize((224, 224)),  
     transforms.ToTensor(),
-    transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762))  # CIFAR-100 的均值和标准差
+    transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762)
 ])
 
 
-# 加载训练集和测试集
-train_dataset = datasets.CIFAR100(root='./boundary_robustness/data', train=True, download=True, transform=transform)
-test_dataset = datasets.CIFAR100(root='./boundary_robustness/data', train=False, download=True, transform=transform)
+train_dataset = datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
+test_dataset = datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
 
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=False)
 
-# 创建新的MobileNet模型实例
 model = ModifiedAlexNet().to(device)
 model_save_path = 'boundary_robustness/models/cifar100/AlexNet_cifar100.pth'
-# 加载模型参数
 model.load_state_dict(torch.load(model_save_path))
-model.eval()  # 切换到评估模式
+model.eval() 
 
 adv_data, labels = PGD_attack.save_pgd_attack(model=model, test_loader=train_loader, epsilon=0.01, alpha=0.001, iters=10)
 
-np.save('boundary_robustness/attackData/Cifar100/AlexNet_datas.npy', adv_data)  # 保存对抗样本
-np.save('boundary_robustness/attackData/Cifar100/AlexNet_labels.npy', labels)   # 保存标签
+np.save('./attackData/Cifar100/AlexNet_datas.npy', adv_data)  # save adversarial samples
+np.save('./attackData/Cifar100/AlexNet_labels.npy', labels)   # save labels
