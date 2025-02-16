4d346eba6b556221a7bfb7e43ca7b7760ac7f111
Jialei Song <95927824+songjialeichenruiyi@users.noreply.github.com>
2025-02-16 17:03:31 +0800

    Update AlexNet_Cifar10_adv_train.py

--- AlexNet_Cifar10_adv_train.py
+++ AlexNet_Cifar10_adv_train.py
@@ -6,37 +6,33 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision
 import torchvision.transforms as transforms
 from torchvision import datasets, models
-# import ROBY
 import RDI
 
-# 检查是否有可用的GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(torch.cuda.is_available())
 
-# 加载torchvision.models中的AlexNet模型，并修改输入层以适应CIFAR-10数据集
 class ModifiedAlexNet(nn.Module):
     def __init__(self, num_classes=10):
         super(ModifiedAlexNet, self).__init__()
         self.model = models.alexnet(weights = None)
-        self.model.features[0] = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)  # 修改输入层以适应CIFAR-10
-        self.model.classifier[6] = nn.Linear(4096, num_classes)  # 修改输出层以适应CIFAR-10的类别数量
+        self.model.features[0] = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.model.classifier[6] = nn.Linear(4096, num_classes)
 
     def forward(self, x):
         return self.model(x)
     
-    
-# 1. 加载训练好的模型
-model = ModifiedAlexNet().to(device)
-model_save_path = 'boundary_robustness/models/cifar10/AlexNet_cifar10_2.pth'
-# 加载模型参数
-model.load_state_dict(torch.load(model_save_path))
-model.eval()  # 切换到评估模式
 
-# 2. 自定义数据集类，用于加载对抗样本
+model = ModifiedAlexNet().to(device)
+model_save_path = './models/cifar10/AlexNet_cifar10_2.pth'
+
+model.load_state_dict(torch.load(model_save_path))
+model.eval()
+
+#Custom dataset class for loading adversarial samples
 class AdversarialDataset(Dataset):
     def __init__(self, adv_data_file, label_file, transform=None):
-        self.adv_data = np.load(adv_data_file)  # 加载对抗样本
-        self.labels = np.load(label_file)       # 加载标签
+        self.adv_data = np.load(adv_data_file)  
+        self.labels = np.load(label_file) 
         self.transform = transform
 
     def __len__(self):
@@ -51,21 +47,19 @@ class AdversarialDataset(Dataset):
         
         return image, label
 
-# 3. 数据加载部分
+
 transform = transforms.Compose([
-    # transforms.RandomCrop(32, padding=4),  # 随机裁剪到 32x32，并在边界填充 4 像素
-    # transforms.RandomHorizontalFlip(),     # 随机水平翻转
-    # transforms.RandomRotation(15),         # 随机旋转 [-15, 15] 度
-    # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # 调整亮度、对比度、饱和度和色调
+    # transforms.RandomCrop(32, padding=4),  
+    # transforms.RandomHorizontalFlip(),    
+    # transforms.RandomRotation(15),         
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.2435, 0.2616))
 ])
 
 # 3. 数据加载部分
 transform3 = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),  # 随机裁剪到 32x32，并在边界填充 4 像素
-    transforms.RandomHorizontalFlip(),     # 随机水平翻转
-    transforms.RandomRotation(15),         # 随机旋转 [-15, 15] 度
-    # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # 调整亮度、对比度、饱和度和色调
+    transforms.RandomCrop(32, padding=4),  
+    transforms.RandomHorizontalFlip(),    
+    transforms.RandomRotation(15),        
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.2435, 0.2616))
 ])
@@ -75,18 +69,17 @@ transform2 = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.2435, 0.2616))
 ])
 
-# 创建对抗数据集
-trainset = AdversarialDataset(adv_data_file="boundary_robustness/attackData/Cifar10/AlexNet_datas.npy", label_file="boundary_robustness/attackData/Cifar10/AlexNet_labels.npy", transform=transform)
+
+trainset = AdversarialDataset(adv_data_file="boundary_robustness/attackData/Cifar10/AlexNet_datas.npy", label_file="./attackData/Cifar10/AlexNet_labels.npy", transform=transform)
 adv_train_loader = DataLoader(trainset, batch_size=64, shuffle=True)
 
-train_dataset = datasets.CIFAR10(root='./boundary_robustness/data', train=True, download=True, transform=transform3)
+train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform3)
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
-test_dataset = datasets.CIFAR10(root='./boundary_robustness/data', train=False, download=True, transform=transform2)
+test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform2)
 test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=False)
 
 
-# 混合数据加载器的生成函数
 def mixed_train_loader(original_loader, adv_loader):
     """
     从 original_loader 和 adv_loader 取全部数据组成混合批次。
