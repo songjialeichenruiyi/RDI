b72b75aae091a175d1c3af950a2dbccf889788f8
Jialei Song <95927824+songjialeichenruiyi@users.noreply.github.com>
2025-02-16 17:48:43 +0800

    Update AlexNet_imageNet_adv.py

--- AlexNet_imageNet_adv.py
+++ AlexNet_imageNet_adv.py
@@ -39,7 +39,7 @@ dataset_path = "./boundary_robustness/data/tiny-imagenet-200"
 train_dir = os.path.join(dataset_path, 'train')
 val_dir = os.path.join(dataset_path, 'val')
 
-# Custom validation dataset
+# Custom validation dataset class
 class TinyImageNetValDataset(Dataset):
     def __init__(self, val_dir, transform=None):
         self.val_dir = val_dir
@@ -47,7 +47,7 @@ class TinyImageNetValDataset(Dataset):
         self.image_paths = []
         self.labels = []
         
-        # 读取 val_annotations.txt 文件
+        # Read the val_annotations.txt file
         annotations_file = os.path.join(val_dir, 'val_annotations.txt')
         with open(annotations_file, 'r') as f:
             for line in f:
@@ -57,11 +57,11 @@ class TinyImageNetValDataset(Dataset):
                 self.image_paths.append(os.path.join(val_dir, 'images', image_name))
                 self.labels.append(label)
         
-        # 获取类别名称到索引的映射
+        # Get category name to index mapping
         label_names = sorted(list(set(self.labels)))
         self.label_to_idx = {label: idx for idx, label in enumerate(label_names)}
         
-        # 将标签转换为索引
+        # Converting labels to indexes
         self.labels = [self.label_to_idx[label] for label in self.labels]
     
     def __len__(self):
@@ -78,22 +78,18 @@ class TinyImageNetValDataset(Dataset):
         return image, label
 
 
-# 加载训练集
 train_dataset = datasets.ImageFolder(train_dir, transform=transform)
 train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=4)
 
-# 加载验证集
 # test_dataset = TinyImageNetValDataset(val_dir, transform=transform_test)
 # test_loader = DataLoader(test_dataset, batch_size=100, shuffle=False, num_workers=4)
 
-# 创建新的MobileNet模型实例
 model = ModifiedAlexNet().to(device)
-model_save_path = 'boundary_robustness/models/imageNet/AlexNet_imageNet_3.pth'
-# 加载模型参数
+model_save_path = './models/imageNet/AlexNet_imageNet_3.pth'
 model.load_state_dict(torch.load(model_save_path))
-model.eval()  # 切换到评估模式
+model.eval()
 
 adv_data, labels = PGD_attack.save_pgd_attack(model=model, test_loader=train_loader, epsilon=0.005, alpha=0.00025, iters=5)
 
-np.save('boundary_robustness/attackData/ImageNet/AlexNet_datas.npy', adv_data)  # 保存对抗样本
-np.save('boundary_robustness/attackData/ImageNet/AlexNet_labels.npy', labels)   # 保存标签
+np.save('boundary_robustness/attackData/ImageNet/AlexNet_datas.npy', adv_data)  # save adversarial samples 
+np.save('boundary_robustness/attackData/ImageNet/AlexNet_labels.npy', labels)   # save labels
