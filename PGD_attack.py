55f0317aa4aef5ec177638fc9193a36891d800d5
Jialei Song <95927824+songjialeichenruiyi@users.noreply.github.com>
2025-02-16 20:47:46 +0800

    Update PGD_attack.py

--- PGD_attack.py
+++ PGD_attack.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-import torchattacks  # 引入 torchattacks 库
+import torchattacks  # Introducing the torchattacks library
 import os
 from torchvision.transforms import ToPILImage
 import numpy as np
@@ -12,20 +12,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# 检查是否有可用的GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# 使用PGD攻击测试模型的性能
+# Test model performance using PGD attack
 def test_with_pgd_attack(model, test_loader, epsilon=0.3, alpha=0.01, iters=40):
     model.eval()
-    attacks = torchattacks.PGD(model, epsilon, alpha, iters)  # 初始化PGD攻击
+    attacks = torchattacks.PGD(model, epsilon, alpha, iters)  # Initialize PGD attacks
 
     correct = 0
     total = 0
 
     for images, labels in test_loader:
         images, labels = images.to(device), labels.to(device)
-        adv_images = attacks(images, labels)  # 生成对抗样本
+        adv_images = attacks(images, labels)  # Generating Adversarial Samples
         outputs = model(adv_images)
         
         _, predicted = torch.max(outputs, 1)
@@ -39,11 +38,11 @@ def test_with_pgd_attack(model, test_loader, epsilon=0.3, alpha=0.01, iters=40):
     accuracy = 100 * correct / total
     print(f'Accuracy under PGD attack: {accuracy:.2f}%')
 
-# 使用PGD攻击测试模型的性能
+# Testing Model Performance with PGD Attack
 def save_pgd_attack(model, test_loader, epsilon=0.3, alpha=0.01, iters=40):
     attack = torchattacks.PGD(model, epsilon, alpha, iters)
     
-    # 4. 生成对抗样本并保存
+    # Generate adversarial samples and save
     adv_data = []
     labels = []
     model.eval()
@@ -51,8 +50,8 @@ def save_pgd_attack(model, test_loader, epsilon=0.3, alpha=0.01, iters=40):
     for images, targets in test_loader:
         images, targets = images.to(device), targets.to(device)
         adv_images = attack(images, targets)
-        adv_data.append(adv_images.cpu().numpy())  # 保存对抗样本
-        labels.append(targets.cpu().numpy())  # 
+        adv_data.append(adv_images.cpu().numpy())
+        labels.append(targets.cpu().numpy())
         size = size + 1
         print(str(size) + '/938')
 
