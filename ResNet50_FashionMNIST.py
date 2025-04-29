0357d2844a84465ec9641b1c59bb459d5a1114c1
Jialei Song <95927824+songjialeichenruiyi@users.noreply.github.com>
2025-04-29 15:05:07 +0800

    Update ResNet50_FashionMNIST.py

--- ResNet50_FashionMNIST.py
+++ ResNet50_FashionMNIST.py
@@ -12,6 +12,7 @@ from torchvision.transforms import ToPILImage
 import torchattacks
 import RFGSM
 import Square_Attack
+import CW_attack
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -121,3 +122,9 @@ PGD_attack.test_with_pgd_attack(loaded_model, test_loader)
 # Testing model performance under Square attack
 # print("Evaluating under Square attack...")
 # Square_Attack.test_with_square_attack(loaded_model, test_loader)
+
+# Testing model performance under CW attack
+# print("Evaluating under CW attack...")
+# CW_attack.test_with_cw_attack(loaded_model, test_loader, 1, 0.01, 50)
+
+
